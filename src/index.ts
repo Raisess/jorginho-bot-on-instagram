c9dc35b5572e73302fd39facd08797b5717a3ba3
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-12 20:09:46 -0300

    refactored main code && added action change option XD

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,15 @@ import { goToPost } from './actions/goToPost';
 import { like, save } from './actions/likeAndSave';
 import { comment } from './actions/comment';
 
-// options
-const timesToComment: number = parseInt(question('times to comment: '));
-const typeOfComment:  string = question('type of comments [male, female, other]: ');
-const translateTo:    string = question('translate comment to another language [eng, ger, jpn, frc]: ');
+// change action type
+const actionType: number = parseInt(question('action type [1 = follow-back, 2 = like/save/comment]: '));
 
-(async () => {
+(async (): Promise<void> => {
 	// browser launch config
 	const browser: any = await launch({ 
-	  args:             ['--window-size=770,720'],
-		headless:         false,
-	  defaultViewport:  null
+	  args:            ['--window-size=770,720'],
+		headless:        false,
+	  defaultViewport: null
 	});
 
 	const page: any = await browser.newPage();
@@ -25,16 +23,41 @@ const translateTo:    string = question('translate comment to another language [
 	// ACTIONS
 	// make login
 	await login(page);
-	// go to post page
-	await goToPost(page);
-	// like the post
-	await like(page);
-	// save the post
-	await save(page);
-	// comment on post
-	await comment(page, translateTo != '' ? translateTo : false, timesToComment, typeOfComment);
-	
+
+	// check the action type
+	switch (actionType) {
+		// TODO: create the follow-back bot followers action
+		case 1:
+			console.log("don't have the first option...");
+
+			break;
+		// if is to like/save/comment
+		case 2:
+			// post to go
+			const postUrl:        string = question('post url: ');
+
+			// comment options
+			const timesToComment: number = parseInt(question('times to comment: '));
+			const typeOfComment:  string = question('type of comments [male, female, other]: ');
+			const translateTo:    string = question('translate comment to another language [eng, ger, jpn, frc]: ');
+
+			// go to post page
+			await goToPost(page, postUrl);
+			// like the post
+			await like(page);
+			// save the post
+			await save(page);
+			// comment on post
+			await comment(page, translateTo != '' ? translateTo : false, timesToComment, typeOfComment);
+
+			break;
+		default:
+			break;
+	}
+
 	// close browser
 	//await browser.close();
+	
+	return;
 })();
 
