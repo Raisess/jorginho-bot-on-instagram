f46f4591236f6f226bb560f40b49462e6e06dac0
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-21 10:22:44 -0300

    separeted interfaces from actions code

---
+++ src/interfaces/IStorage.ts
@@ -0,0 +1,9 @@
+export interface IStorage {
+	followersNumber:  number;
+	followers:        Array<string>;
+	followingNumber:  number;
+	following:        Array<string>;
+	notReciprocal:    Array<string>;
+	difference:       number;
+}
+
