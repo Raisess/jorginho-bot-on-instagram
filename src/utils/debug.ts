c4be2255f30d555e4b038e39d8d0a148e84c75b5
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-21 11:02:21 -0300

    updated params support

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,4 +1,4 @@
 import { debug } from '../config.json';
 
-export const log = (toLog: any): void => debug ? console.log(toLog) : undefined;
+export const log = (...toLog: any): void => debug ? console.log(toLog) : undefined;
 
